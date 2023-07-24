7b2a426b82c6165763f033a6a6558f2b57f76d91
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-07-24 10:35:07 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverActions: true,
+    },
+    images: {
+        domains: ['lh3.googleusercontent.com', "firebasestorage.googleapis.com"], // Add more domains if needed, separated by commas
+    },
+}
 
 module.exports = nextConfig
