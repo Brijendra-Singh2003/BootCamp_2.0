a24216696c7e0801d0bf5fca943661a94dbcebf1
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-08-20 11:15:13 +0530

    added favicon

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,28 @@ import Navbar from '@/components/Navbar/Navbar'
 import { options } from "./api/auth/[...nextauth]/options"
 import { getServerSession } from "next-auth"
 
+export const metadata = {
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon/favicon-16x16.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/favicon/apple-touch-icon.png',
+    },
+  ],
+}
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default async function RootLayout({ children }) {
--- app/page.js
+++ app/page.js
@@ -30,6 +30,25 @@ export default async function Home() {
 export const metadata = {
   title: "CSE Bootcamp 2.0 - Home",
   description: "A palce to clill",
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon/favicon-16x16.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/favicon/apple-touch-icon.png',
+    },
+  ]
 };
 
 // {"name":"Brijendra Singh","email":"b122041@iiit-bh.ac.in","image":"https://lh3.googleusercontent.com/a/AAcHTteV-zE6NIOj8A5zk2Wo_aglZgQzO5m5qLr_n9-O6smA=s96-c"}
