a944f250087c41b8549450fae333c34331bab0dd
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-08-21 21:39:44 +0530

    basic formating in navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import styles from "./Navbar.module.css";
+import  "./Navbar.css";
 import Link from "next/link";
 import Image from "next/image";
 import { signOut } from 'next-auth/react';
@@ -14,69 +14,61 @@ export default function Navbar({ user }) {
   }
 
   return (
-    <header
-      className={`${styles.header} w-full top-0`}
-    >
-      <Link className="sm:ml-[4%]" href={"https://www.iiit-bh.ac.in/"}>
-        <Image src={"/assets/logo.png"} height={140} width={140} alt="IIIT BBSR logo"/>
+    <header>
+
+      { showMenu ? <div className="backdrop" onClick={toggle}></div> : null }
+
+      <Link className="logo" href="https://www.iiit-bh.ac.in/">
+        <Image src="/assets/logo.png" height={140} width={140} alt="IIIT BBSR logo"/>
       </Link>
-      {showMenu && <div className={styles.backdrop} onClick={toggle}></div>}
-      <div className={styles.mobileLink} onClick={toggle}>
-        <svg
-          className="w-8 h-8 text-gray-800 dark:text-white m-5 scale-100"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 17 14"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M1 1h15M1 7h15M1 13h15"
-          />
-        </svg>
-        <div
-          className={`${styles.links} ${showMenu ? styles.show : styles.hide}`}
-        >
-          <Link href={"/"}>Home</Link>
+
+      <div className="mobile-links" onClick={toggle}>
+        
+        <svg className="menu-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14" ><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" /></svg>
+
+        <div className={ "dropDown " + (showMenu ? "show" : "hide") }>
+
+          <Link href="/">Home</Link>
           {user ? (
             <>
-              <Link href={"/students/2023"}>2023</Link>
-              <Link href={"/students/2022"}>2022</Link>
-              <Link href={"/about"}>About</Link>
-              <Link href={"/societies"}>Societies</Link>
+              <Link href="/students/2023">2023</Link>
+              <Link href="/students/2022">2022</Link>
+              <Link href="/about">About</Link>
+              <Link href="/societies">Societies</Link>
               <Link href={{ pathname: "/profile", query: user }}>Profile</Link>
-              <a className=" cursor-pointer" onClick={()=>{signOut()}}>Signout</a>
+              <a className="cursor-pointer" onClick={()=>{signOut()}}>Signout</a>
             </>
           ) : (
             <>
-              <Link href={"/about"}>About</Link>
-              <Link href={"/api/auth/signin"}>Login</Link>
+              <Link href="/societies">Societies</Link>
+              <Link href="/about">About</Link>
+              <Link href="/api/auth/signin">Login</Link>
             </>
           )}
+
         </div>
       </div>
-      <div className={styles.navLink}>
-        <Link href={"/"}>Home</Link>
+
+      <div className="desktop-links">
+        <Link href="/">Home</Link>
         {user ? (
           <>
-            <Link href={"/students/2023"}>2023</Link>
-            <Link href={"/students/2022"}>2022</Link>
-            <Link href={"/societies"}>Societies</Link>
-            <Link href={"/about"}>About</Link>
+            <Link href="/students/2023">2023</Link>
+            <Link href="/students/2022">2022</Link>
+            <Link href="/about">About</Link>
+            <Link href="/societies">Societies</Link>
             <Link href={{ pathname: "/profile", query: user }}>Profile</Link>
-            <button className={styles.signout} onClick={()=>{signOut()}}>Signout</button>
+            <button className="signout" onClick={()=>{signOut()}}>Signout</button>
           </>
         ) : (
           <>
-            <Link href={"/societies"}>Societies</Link>
-            <Link href={"/about"}>About</Link>
-            <Link href={"/api/auth/signin"}>Login</Link>
+            <Link href="/societies">Societies</Link>
+            <Link href="/about">About</Link>
+            <Link href="/api/auth/signin">Login</Link>
           </>
         )}
       </div>
+
     </header>
   );
 }
