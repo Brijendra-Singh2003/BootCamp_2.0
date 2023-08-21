1ef26c02bdd6d5e7916407170c3fc8f9d66cc63b
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-08-21 16:14:19 +0530

    siciety responsive

--- app/societies/page.js
+++ app/societies/page.js
@@ -13,29 +13,31 @@ function Society() {
       </p>
     </div>
 
-    <Lottie
-        className='left-card'
-        src="https://lottie.host/d50c2b17-e9cb-48fd-b398-ebc016684817/9xMhusflLl.json"/>
+    <div className="container">
+      <Lottie
+          className='left-card'
+          src="https://lottie.host/d50c2b17-e9cb-48fd-b398-ebc016684817/9xMhusflLl.json"/>
 
-      <Lottie
-        className='right-card'
-        src="https://lottie.host/ffb8224c-0789-4a89-ac59-c85c8958541c/aof29xsnyr.json"/>
+        <Lottie
+          className='right-card'
+          src="https://lottie.host/ffb8224c-0789-4a89-ac59-c85c8958541c/aof29xsnyr.json"/>
 
-      <Lottie
-        className='left-card'
-        src="https://lottie.host/360f78e9-4acb-4728-9404-d62c4fda20b4/pJVVstSbOg.json"/>
+        <Lottie
+          className='left-card'
+          src="https://lottie.host/360f78e9-4acb-4728-9404-d62c4fda20b4/pJVVstSbOg.json"/>
 
-      <Lottie
-        className='right-card'
-        src="https://lottie.host/c7561713-c5a7-4cd3-becb-188099674eec/PFXmeRb60Y.json"/>
+        <Lottie
+          className='right-card'
+          src="https://lottie.host/c7561713-c5a7-4cd3-becb-188099674eec/PFXmeRb60Y.json"/>
 
-      <Lottie
-        className='left-card'
-        src="https://lottie.host/44e21f05-0bf3-4615-9ee1-9fd688806b44/h5NAGTRDW5.json"/>
-      
-      <Lottie
-        className='right-card'
-        src="https://lottie.host/b53a03ab-7407-4a8c-81bc-bd26d8355484/oKFOwD0sSA.json"/>
+        <Lottie
+          className='left-card'
+          src="https://lottie.host/44e21f05-0bf3-4615-9ee1-9fd688806b44/h5NAGTRDW5.json"/>
+        
+        <Lottie
+          className='right-card'
+          src="https://lottie.host/b53a03ab-7407-4a8c-81bc-bd26d8355484/oKFOwD0sSA.json"/>
+      </div>
     </>
   )
 }
