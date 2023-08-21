057f6ede8866e4175b153d0c6876e7b92956b26d
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-08-21 12:44:34 +0530

    remover 300px cap on animations

--- components/animations/Lottie.js
+++ components/animations/Lottie.js
@@ -9,7 +9,6 @@ export default function Lottie({className, src, id}) {
             src={src}
             background="transparent"
             speed="1"
-            style={{ width: "300px", height: "300px" }}
             loop
             autoplay
         ></lottie-player>
