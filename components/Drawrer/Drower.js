dae03975d684503eef427048bde5a45abc0b5ee4
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-08-22 16:59:22 +0530

    push

--- components/Drawrer/Drower.js
+++ components/Drawrer/Drower.js
@@ -1,27 +1,32 @@
 "use client";
 
-import React, { useState } from "react";
 import { Drawer } from "vaul";
+import "./drawer.css"
 
-export function Drower2() {
+export function DrowerTrigger({children}) {
+    return (
+        <Drawer.Trigger>
+            {children}
+        </Drawer.Trigger>
+    )
+}
+
+export function Drower2({children}) {
 
     return (
         <Drawer.Root defaultOpen>
-            <Drawer.Trigger asChild>
-                <button>Open Drawer</button>
-            </Drawer.Trigger>
             <Drawer.Portal>
                 <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-                <Drawer.Content className=" flex flex-col rounded-[40px] mt-24 fixed bottom-10 top-10 left-10 right-10">
-                    <Drawer.Trigger asChild>
-                        <button className=" text-black absolute top-0 right-0 border-0 bg-gray-500 rounded-full">
-                            X
-                        </button>
-                    </Drawer.Trigger>
-                    <div className="p-4 bg-white rounded-[20px] flex-1">
+                <Drawer.Content className=" flex flex-col rounded-[40px] mt-24 fixed bottom-0 top-10 left-10 right-10">
+                    <div className="flex-1 student-card">
                         <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
+                        <Drawer.Trigger asChild>
+                            <button className="close-button">
+                                X
+                            </button>
+                        </Drawer.Trigger>
                         <div className="max-w-md mx-auto">
-                            
+                            {children}
                         </div>
                     </div>
                 </Drawer.Content>
