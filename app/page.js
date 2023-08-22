3b2fa68b38d93471e7cc2922cd137b730a8464cf
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-08-22 21:59:53 +0530

    home page animetion

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default async function Home() {
   return (
     <>
       <lottie-player
-        className='h-[80%] mt-40 w-[80%] m-auto lottie'
+        className='h-[40%] mt-10 w-[80%] m-auto lottie'
         src="/animations/helloworld.json"
         background="transparent"
         speed="1"
