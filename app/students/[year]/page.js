47272d881d4cb4ad26803301ae32f8312c91037e
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-08-23 00:21:36 +0530

    list added

--- app/students/[year]/page.js
+++ app/students/[year]/page.js
@@ -1,9 +1,8 @@
 import Image from 'next/image';
 import Retry from '@/components/Retry';
 import "./students.css"
-import StudentList from '@/components/studentList/StudentList';
 import sampleData from './sampleData';
-import { Drower, DrowerRoot, DrowerTrigger } from '@/components/Drawrer/Drower';
+import { Drower } from '@/components/Drawrer/Drower';
 
 // run "npm i" in terminal to install vaul before running
 
@@ -12,39 +11,37 @@ export const metadata = {
     description: "About your peers",
 };
 
-const curr = {
-    _id: "64e1f12024452bbb960c865e",
-    id: "b122041",
-    name: "Brijendra Singh",
-    about: "",
-    state: "mp",
-    city: "pali",
-    instagram: "https://www.instagram.com/",
-    github: "https://github.com/",
-    linkedin: "https://www.linkedin.com/",
-    image: "https://firebasestorage.googleapis.com/v0/b/cse-bootcamp-auth.appspot.com/o/images%2Fb122041?alt=media&token=ddedfdf7-8c2a-4927-b945-ee1c003b1055",
-    year: 2,
-    __v: 0
-  }
+// const curr = {
+//     _id: "64e1f12024452bbb960c865e",
+//     id: "b122041",
+//     name: "Brijendra Singh",
+//     about: "",
+//     state: "mp",
+//     city: "pali",
+//     instagram: "https://www.instagram.com/",
+//     github: "https://github.com/",
+//     linkedin: "https://www.linkedin.com/",
+//     image: "https://firebasestorage.googleapis.com/v0/b/cse-bootcamp-auth.appspot.com/o/images%2Fb122041?alt=media&token=ddedfdf7-8c2a-4927-b945-ee1c003b1055",
+//     year: 2,
+//     __v: 0
+// }
 
 export default async function StudentPage({params:{year}}) {
 
-    let studentsList = [sampleData];
+    let studentsList = sampleData;
     let err = false;
 
-    // try {
-    //     const response = await fetch(`${process.env.HOST}/api/db/getall?year=${year[3]}`,{ cache: 'no-store' });
-    //     studentsList = await response.json();
-    // } catch (error) {
-    //     return <div className=" mt-52"><h1>Something went wrong</h1><Retry>Retry</Retry></div>
-    // }
+    try {
+        const response = await fetch(`${process.env.HOST}/api/db/getall?year=${year[3]}`,{ cache: 'no-store' });
+        studentsList = await response.json();
+    } catch (error) {
+        return <div className=" mt-52"><h1>Something went wrong</h1><Retry>Retry</Retry></div>
+
+    }
 
     return (
-        <DrowerRoot>
-            <DrowerTrigger>
-                open card
-            </DrowerTrigger>
-            <Drower {...curr} />
-        </DrowerRoot>
+        <>
+            <Drower userlist={studentsList}/>
+        </>
     )
 }
--- components/Drawrer/Drower.js
+++ components/Drawrer/Drower.js
@@ -4,46 +4,50 @@ import { Drawer } from "vaul";
 import "./drawer.css"
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
-export function DrowerTrigger({children}) {
-    return (
-        <Drawer.Trigger>
-            {children}
-        </Drawer.Trigger>
-    )
-}
+export function Drower({userlist}) {
 
-export function DrowerRoot({children}) {
+    const [active, setActive] = useState({});
+    const {image, id, name, city, state, about, instagram, linkedin, github} = active;
+
     return (
         <Drawer.Root>
-            {children}
-        </Drawer.Root>
-    )
-}
 
-export function Drower({image, id, name, city, state, about, instagram, linkedin, github}) {
+            <Drawer.Trigger className="trigger">
+                {userlist.map((curr)=>{
+                    return(
+                        <div className="student-card" onClick={()=>setActive(curr)}>
+                            <Image src={curr.image} height={70} width={70} />
+                            <div>
+                                <h1>{curr.name}</h1>
+                                <p>{curr.city+', '+curr.state}</p>
+                            </div>
+                            <span>ID #{curr.id.substring(4, 7)}</span>
+                        </div>
+                    )
+                })}
+            </Drawer.Trigger>
 
-    return (
-        <>
-        <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-        <Drawer.Content className="drower-container">
-            <div className="flex-1 drower-card">
-                <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-3" />
-                <div className="max-w-md mx-auto">
-                    <Image className="drower-image" src={image} height={200} width={400} />
-                    <span className="drower-id">ID: #{id?.substring(4, 7)}</span>
-                    <h1 className="drower-name">{name?.toUpperCase()}</h1>
-                    <p className="drower-city">{(city+', '+state).toUpperCase()}</p>
-                    <div className="drower-about">{about}</div>
-                    <div className="drower-logo-container">
-                        {instagram && <Link target="_blank" href={instagram} ><Image src={'/logo/instagram-logo.png'} height={30} width={30}/></Link>}
-                        {linkedin && <Link target="_blank" href={linkedin} ><Image className=" bg-transparent" src={'/logo/linkedin-logo.png'} height={30} width={30}/></Link>}
-                        {github && <Link target="_blank" href={github} ><Image src={'/logo/github-logo.png'} height={30} width={30}/></Link>}
-                        {id && <Link target="_blank" href={`https://mail.google.com/mail/?view=cm&to=${id}@iiit-bh.ac.in`}><Image src={'/logo/mail-logo.png'} height={30} width={30}/></Link>}
+            <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+            <Drawer.Content className="drower-container">
+                {/* <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-1" /> */}
+                <div className="flex-1 drower-card">
+                    <div className="max-w-md mx-auto">
+                        <Image className="drower-image" src={image} height={200} width={400} />
+                        <span className="drower-id">ID: #{id?.substring(4, 7)}</span>
+                        <h1 className="drower-name">{name?.toUpperCase()}</h1>
+                        <p className="drower-city">{(city+', '+state).toUpperCase()}</p>
+                        <div className="drower-about">{about?.toUpperCase()}</div>
+                        <div className="drower-logo-container">
+                            {instagram && <Link target="_blank" href={instagram} ><Image src={'/logo/instagram-logo.png'} height={30} width={30}/></Link>}
+                            {linkedin && <Link target="_blank" href={linkedin} ><Image className=" bg-transparent" src={'/logo/linkedin-logo.png'} height={30} width={30}/></Link>}
+                            {github && <Link target="_blank" href={github} ><Image src={'/logo/github-logo.png'} height={30} width={30}/></Link>}
+                            {id && <Link target="_blank" href={`https://mail.google.com/mail/?view=cm&to=${id}@iiit-bh.ac.in`}><Image src={'/logo/mail-logo.png'} height={30} width={30}/></Link>}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </Drawer.Content>
-        </>
+            </Drawer.Content>
+        </Drawer.Root>
     );
 }
