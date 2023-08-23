d3c5d262cb24194078f0796f30e6f5ba1ad4b2b5
Akataruka <121378361+Akataruka@users.noreply.github.com>
2023-08-23 20:07:09 +0530

    responsive card

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import Navbar from '@/components/Navbar/Navbar'
 import { options } from "./api/auth/[...nextauth]/options"
 import { getServerSession } from "next-auth"
 import Script from 'next/script'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
 export const metadata = {
   icons: [
@@ -43,7 +43,7 @@ export default async function RootLayout({ children }) {
         <main>
           {children}
         </main>
-        <ToastContainer/>
+        {/* <ToastContainer/> */}
         <Script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js" defer/>
       </body>
     </html>
