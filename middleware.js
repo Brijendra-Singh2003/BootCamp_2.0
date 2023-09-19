73506c86d0128633510d6716821df62e2c830c60
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-09-19 22:28:45 +0530

    society secured

--- middleware.js
+++ middleware.js
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/" ,"/profile", "/students/:path*", "/society", "/about"] }
+export const config = { matcher: ["/" ,"/profile", "/students/:path*", "/societies", "/about"] }
