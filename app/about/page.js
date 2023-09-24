66c45a08e592ab4d9db2dcce8dec89550a3c8a7f
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-09-24 17:54:28 +0530

    about card

--- app/about/page.js
+++ app/about/page.js
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import anime from "@/public/animations/about.json";
 import Lottie from '@/components/animations/Lottie';
+import DevCard from '@/components/devCard/DevCard';
 
 const lilitaOne = Roboto({ subsets: ['latin'], weight: "700" });
 const lato = Lato({ subsets: ['latin'], weight: "700" });
@@ -21,50 +22,96 @@ export default async function About() {
       </p>
     </div>
     <div className={"teams no-hlt " + lilitaOne.className}>
-      <div className="about-card small no-select">
-          <p>OUR TEAM</p>
-          <div className="Acont flex gap-4 flex-wrap items-center justify-center">
-            <div className="Acard border border-blue-100 rounded-2xl p-4">
-              <h1 className='scale-[1.2]'>Brijendra Singh</h1>
-              <h1>B122041</h1>
-              <h1>Frontend, Backend</h1>
+        <h1 className={"desc "}>OUR TEAM</h1>
+      <div className="flex gap-6 small no-select w-full justify-center items-center">
+        <DevCard 
+          src="/assets/default-avatar.png"
+          name="Brijendra Singh"
+          id="B122041"
+        >
+          <ul>
+          <li>- Backend</li>
+            <li>- Frontend</li>
+          </ul>
+        </DevCard>
+        <DevCard 
+          src="/assets/default-avatar.png"
+          name="Ashutosh Kataruka"
+          id="B122029"
+          >
+          <ul>
+            <li>- Frontend</li>
+            <li>- Design</li>
+          </ul>
+        </DevCard>
+        <DevCard 
+          src="/assets/default-avatar.png"
+          name="Abhik Sundar Sahu"
+          id="B122001"
+        >
+          <ul>
+            <li>- Research</li>
+            <li>- Content</li>
+          </ul>
+        </DevCard>
+          {/* <div className="Acont flex gap-4 flex-wrap items-center justify-center">
+            <div className="Acard flex border border-blue-100 rounded-2xl p-4">
+              <div className='width-[60%] text-left pl-4'>
+              <h1 className='name'>Brijendra Singh</h1>
+                <h1>B122041</h1>
+                <h1>Frontend, Backend</h1>
+              </div>
             </div>
-            <div className="Acard border border-blue-100 rounded-2xl p-4">
-              <h1 className='scale-[1.2]'>Asutosh Kataruka</h1>
-              <h1>B122029</h1>
-              <h1>Frontend, Design</h1>
+            <div className="Acard flex border border-blue-100 rounded-2xl p-4">
+              <div className='width-[60%] text-left pl-4'>
+              <h1 className='name'>Asutosh Kataruka</h1>
+                <h1>B122029</h1>
+                <h1>Frontend, Design</h1>
+              </div>
             </div>
-            <div className="Acard border border-blue-100 rounded-2xl p-4">
-              <h1 className='scale-[1.2]'>Abhik Sundar Sahu</h1>
-              <h1>B122001</h1>
-              <h1>Research, Content</h1>
+            <div className="Acard flex border border-blue-100 rounded-2xl p-4">
+              <div className='width-[60%] text-left pl-4'>
+              <h1 className='name'>Abhik Sundar Sahu</h1>
+                <h1>B122001</h1>
+                <h1>Research, Content</h1>
+              </div>
             </div>
-            <div className="Acard border border-blue-100 rounded-2xl p-4">
-              <h1 className='scale-[1.2]'>Anirudh Praadhan</h1>
-              <h1>B122018</h1>
-              <h1>Content, Design</h1>
+            <div className="Acard flex border border-blue-100 rounded-2xl p-4">
+              <div className='width-[60%] text-left pl-4'>
+              <h1 className='name'>Anirudh Praadhan</h1>
+                <h1>B122018</h1>
+                <h1>Content, Design</h1>
+              </div>
             </div>
-            <div className="Acard border border-blue-100 rounded-2xl p-4">
-              <h1 className='scale-[1.2]'>Amit Behera</h1>
-              <h1>B122015</h1>
-              <h1>Content</h1>
+            <div className="Acard flex border border-blue-100 rounded-2xl p-4">
+              <div className='width-[60%] text-left pl-4'>
+              <h1 className='name'>Amit Behera</h1>
+                <h1>B122015</h1>
+                <h1>Content</h1>
+              </div>
             </div>
-            <div className="Acard border border-blue-100 rounded-2xl p-4">
-              <h1 className='scale-[1.2]'>Abinas Ojha</h1>
-              <h1>B122004</h1>
-              <h1>content</h1>
+            <div className="Acard flex border border-blue-100 rounded-2xl p-4">
+              <div className='width-[60%] text-left pl-4'>
+              <h1 className='name'>Abinas Ojha</h1>
+                <h1>B122004</h1>
+                <h1>content</h1>
+              </div>
             </div>
-            <div className="Acard border border-blue-100 rounded-2xl p-4">
-              <h1 className='scale-[1.2]'>Arya Singh Bais</h1>
-              <h1>B122025</h1>
-              <h1>Design</h1>
+            <div className="Acard flex border border-blue-100 rounded-2xl p-4">
+              <div className='width-[60%] text-left pl-4'>
+              <h1 className='name'>Arya Singh Bais</h1>
+                <h1>B122025</h1>
+                <h1>Design</h1>
+              </div>
             </div>
-            <div className="Acard border border-blue-100 rounded-2xl p-4">
-              <h1 className='scale-[1.2]'>Aditya Narayan</h1>
-              <h1>B122008</h1>
-              <h1>Graphic Designer</h1>
+            <div className="Acard flex border border-blue-100 rounded-2xl p-4">
+              <div className='width-[60%] text-left pl-4'>
+              <h1 className='name'>Aditya Narayan</h1>
+                <h1>B122008</h1>
+                <h1>Graphic Designer</h1>
+              </div>
             </div>
-          </div>
+          </div> */}
       </div>
     </div>
     <p className={'stack no-select '+lilitaOne.className}>Tech Stack Used</p>
