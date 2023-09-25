49ff6ba81e06627446332deb0fe8565a20dc6a40
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-09-25 18:24:17 +0530

    society card

--- components/button/SignInButton.js
+++ components/button/SignInButton.js
@@ -3,9 +3,12 @@
 import { signIn } from 'next-auth/react'
 
 export default function SignInButton() {
+    function foo() {
+        signIn('google');
+    }
     return (
         <div className="innerleft">
-            <button id="sign" onClick={() => {signIn('google')}}>Sign In</button>
+            <button id="sign" onTouchStart={foo} onMouseDown={foo}>Sign In</button>
         </div>
     )
 }
--- components/society_card/society_card.js
+++ components/society_card/society_card.js
@@ -1,14 +1,20 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
 import "./society_card.css"
 
-export async function Society_card({heading,src,instagram,para}) {
+export function Society_card({heading,src,instagram,para}) {
+    const [active, setActive] = useState(true);
+    function foo(e) {
+        setActive(prevState => !prevState);
+    }
     return (
         <>
-            <div className='card-placeholder'>
+            <div onClick={foo} className='card-placeholder'>
                 <div className='card'>
-                    <div className="front">
+                    <div className={"front "+(active ? "" : "snap")}>
                         <Image
                             className='no-select img '
                             priority
@@ -18,7 +24,7 @@ export async function Society_card({heading,src,instagram,para}) {
                             alt='' />
                         <h1 className='text-black no-select'>{heading}</h1>
                     </div>
-                    <div className="back ">
+                    <div className={"back "+(active ? "" : "blip")}>
                         <Link className='btn no-select' target='_blank'
                             href={instagram} ><h1>VIEW PAGE</h1></Link>
                         <p className='para no-select absolute top-[16%] text-black text-justify'>{para}</p>
