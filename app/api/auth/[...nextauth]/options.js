ae3dbb5b7cbdf3d7a9a3429e5aaec9cc855b3e9d
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-09-25 23:49:30 +0530

    access

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -11,7 +11,19 @@ export const options = {
         async signIn({ account, profile }) {
             const [id, provider] = profile.email.split('@');
             if (account.provider === "google") {
-                return profile.email_verified && ( provider === "iiit-bh.ac.in" && (id[1] === '1' || id === 'b222032' || id === 'b322020' || id === 'b422001' || id === 'b322037' || id === 'b522034' || id === 'b322006'));
+                return profile.email_verified && (
+                    provider === "iiit-bh.ac.in" && 
+                    ( id[1] === '1' 
+                    || id === 'b222032' 
+                    || id === 'b322020' 
+                    || id === 'b422001' 
+                    || id === 'b322037' 
+                    || id === 'b522034' 
+                    || id === 'b422019' 
+                    || id === 'b322035' 
+                    || id === 'b322029' 
+                    || id === 'b222010' 
+                    || id === 'b322006'));
             }
             return;
         },
