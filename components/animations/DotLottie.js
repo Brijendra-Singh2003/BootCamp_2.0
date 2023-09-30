c5893459bdb600713841ab5edb67262fccc40ed0
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-09-30 10:31:34 +0530

    text size fix

---
+++ components/animations/DotLottie.js
@@ -0,0 +1,13 @@
+import '@dotlottie/player-component';
+import { DotLottiePlayer } from '@dotlottie/player-component';
+
+export default function DotLottie({src}) {
+  return (""
+      // <DotLottiePlayer
+      //   src={src}
+      //   autoplay
+      //   loop
+      //   style={{ height: '100%', width: '100%' }}
+      // />
+  );
+}
