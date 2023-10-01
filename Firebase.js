3143b312d3a59584e62eaf54f2c5e26d0503eaaf
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-10-02 00:05:11 +0530

    db changed

--- Firebase.js
+++ Firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY,
-  authDomain: process.env.FIREBASE_AUTHDOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASEURL,
-  projectId: process.env.FIREBASE_PROJECTID,
-  storageBucket: "cse-bootcamp-auth.appspot.com",
-  messagingSenderId: Number.parseInt(process.env.FIREBASE_MESSIGINGSENDERID),
-  appId: process.env.FIREBASE_APPID
+  apiKey: "AIzaSyCAD4rDcTvVYurj0y8t4MIeoBjXJWjiDhI",
+  authDomain: "react-http-bb485.firebaseapp.com",
+  databaseURL: "https://react-http-bb485-default-rtdb.firebaseio.com",
+  projectId: "react-http-bb485",
+  storageBucket: "react-http-bb485.appspot.com",
+  messagingSenderId: "277021529270",
+  appId: "1:277021529270:web:5de5c6023175993bd74217"
 };
 
 const app = initializeApp(firebaseConfig);
