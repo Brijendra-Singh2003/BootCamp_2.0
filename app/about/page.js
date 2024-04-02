8509d1e9f0f2b38e184e952908a74950d0a406ab
Brijendra-Singh2003 <brijendra0369@gmail.com>
2024-04-02 17:55:50 +0530

    dev card update

--- app/about/page.js
+++ app/about/page.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import "./page.css"
-import { Roboto, Lato } from 'next/font/google'
-import Link from 'next/link';
-import Image from 'next/image';
-import DevCard from '@/components/devCard/DevCard';
+import React from "react";
+import "./page.css";
+import { Roboto, Lato } from "next/font/google";
+import Link from "next/link";
+import Image from "next/image";
+import DevCard from "@/components/devCard/DevCard";
 // import DotLottie from '@/components/animations/DotLottie';
 
-const lilitaOne = Roboto({ subsets: ['latin'], weight: "700" });
-const lato = Lato({ subsets: ['latin'], weight: "700" });
+const lilitaOne = Roboto({ subsets: ["latin"], weight: "700" });
+const lato = Lato({ subsets: ["latin"], weight: "700" });
 
 export default async function About() {
   return (
     <>
       <h1 className={lilitaOne.className + " no-select title m-0"}>ABOUT US</h1>
-      <Image src="/assets/team.png" height={400} width={600} className="about-middle-card" alt=''/>
+      <Image
+        src="/assets/team.png"
+        height={400}
+        width={600}
+        className="about-middle-card"
+        alt=""
+      />
 
       <div className={"no-select " + lato.className}>
         <p className="desc">
-          This bootcamp is made with lots of love and hard work by our entire team working behind this website and it would not have been possible without the support of the entire 2022 - 26 CSE batch.
+          This bootcamp is made with lots of love and hard work by our entire
+          team working behind this website and it would not have been possible
+          without the support of the entire 2022 - 26 CSE batch.
         </p>
       </div>
       <div className={"teams no-hlt  " + lilitaOne.className}>
@@ -63,25 +71,16 @@ export default async function About() {
               <li>- Content Writer</li>
             </ul>
           </DevCard>
-          
-          
-          <DevCard
-            src="/assets/aditya.jpeg"
-            name="Aditya Narayan"
-            id="B122008"
-          >
+
+          <DevCard src="/assets/aditya.jpeg" name="Aditya Narayan" id="B122008">
             <ul>
               <li>- Graphics Designer</li>
               <li>- Video Editor</li>
             </ul>
           </DevCard>
-          <DevCard
-            src="/assets/abhi.jpeg"
-            name="Abinas Ojha"
-            id="B122004"
-          >
+          <DevCard src="/assets/abhi.jpeg" name="Abinas Ojha" id="B122004">
             <ul>
-            <li>- Video Editor</li>
+              <li>- Video Editor</li>
             </ul>
           </DevCard>
           <DevCard
@@ -93,108 +92,118 @@ export default async function About() {
               <li>- Video Editor</li>
             </ul>
           </DevCard>
-          <DevCard
-            src="/assets/amit.jpeg"
-            name="Amit Behera"
-            id="B122015"
-          >
+          <DevCard src="/assets/amit.jpeg" name="Amit Behera" id="B122015">
             <ul>
               <li>- Content Writer</li>
             </ul>
           </DevCard>
-          <DevCard
-            src="/assets/arya.jpeg"
-            name="Arya Singh Bais"
-            id="B122025"
-          >
+          <DevCard src="/assets/arya.jpeg" name="Arya Singh Bais" id="B122025">
             <ul>
               <li>- UI/UX Designer</li>
             </ul>
           </DevCard>
-          
-          
-          
-
-          {/* <div className="Acont flex gap-4 flex-wrap items-center justify-center">
-            <div className="Acard flex border border-blue-100 rounded-2xl p-4">
-              <div className='width-[60%] text-left pl-4'>
-              <h1 className='name'>Brijendra Singh</h1>
-                <h1>B122041</h1>
-                <h1>Frontend, Backend</h1>
-              </div>
-            </div>
-            <div className="Acard flex border border-blue-100 rounded-2xl p-4">
-              <div className='width-[60%] text-left pl-4'>
-              <h1 className='name'>Asutosh Kataruka</h1>
-                <h1>B122029</h1>
-                <h1>Frontend, Design</h1>
-              </div>
-            </div>
-            <div className="Acard flex border border-blue-100 rounded-2xl p-4">
-              <div className='width-[60%] text-left pl-4'>
-              <h1 className='name'>Abhik Sundar Sahu</h1>
-                <h1>B122001</h1>
-                <h1>Research, Content</h1>
-              </div>
-            </div>
-            <div className="Acard flex border border-blue-100 rounded-2xl p-4">
-              <div className='width-[60%] text-left pl-4'>
-              <h1 className='name'>Anirudh Praadhan</h1>
-                <h1>B122018</h1>
-                <h1>Content, Design</h1>
-              </div>
-            </div>
-            <div className="Acard flex border border-blue-100 rounded-2xl p-4">
-              <div className='width-[60%] text-left pl-4'>
-              <h1 className='name'>Amit Behera</h1>
-                <h1>B122015</h1>
-                <h1>Content</h1>
-              </div>
-            </div>
-            <div className="Acard flex border border-blue-100 rounded-2xl p-4">
-              <div className='width-[60%] text-left pl-4'>
-              <h1 className='name'>Abinas Ojha</h1>
-                <h1>B122004</h1>
-                <h1>content</h1>
-              </div>
-            </div>
-            <div className="Acard flex border border-blue-100 rounded-2xl p-4">
-              <div className='width-[60%] text-left pl-4'>
-              <h1 className='name'>Arya Singh Bais</h1>
-                <h1>B122025</h1>
-                <h1>Design</h1>
-              </div>
-            </div>
-            <div className="Acard flex border border-blue-100 rounded-2xl p-4">
-              <div className='width-[60%] text-left pl-4'>
-              <h1 className='name'>Aditya Narayan</h1>
-                <h1>B122008</h1>
-                <h1>Graphic Designer</h1>
-              </div>
-            </div>
-          </div> */}
         </div>
       </div>
-      <p className={'stack no-select ' + lilitaOne.className}>Tech Stack Used</p>
+      <p className={"stack no-select " + lilitaOne.className}>
+        Tech Stack Used
+      </p>
       <div className="tech-box">
-        <div className="tech-box-child">
-          <div className="img no-select"><Image height={400} width={400} src="/stack/html.png" alt="ERROR 404"/></div>
-          <div className="img no-select"><Image height={400} width={400} src="/stack/css-3.png" alt="ERROR 404"/></div>
-          <div className="img no-select"><Image height={400} width={400} src="/stack/js.png" alt="ERROR 404"/></div>
-          <div className="img no-select"><Image height={400} width={400} src="/stack/node.png" alt="ERROR 404"/></div>
-          <div className="img no-select"><Image height={400} width={400} src="/stack/react2.png" alt="ERROR 404"/></div>
-        </div>
-        <div className="tech-box-child">
-          <div className="img no-select"><Image className='scale-[1.3]' height={400} width={400} src="/stack/Next.js.png" alt="ERROR 404"/></div>
-          <div className="img no-select"><Image height={400} width={400} src="/stack/git.png" alt="ERROR 404"/></div>
-          <div className="img no-select"><Image height={400} width={400} src="/stack/github.png" alt="ERROR 404"/></div>
-          <div className="img no-select"><Image height={400} width={400} src="/stack/mongo.png" alt="ERROR 404"/></div>
-          <div className="img no-select"><Image height={400} width={400} src="/stack/azure.png" alt="ERROR 404"/></div>
+        <div className="grid grid-cols-5 md:grid-cols-10 gap-4 max-w-4xl">
+          <div className="img no-select">
+            <Image
+              height={400}
+              width={400}
+              src="/stack/html.png"
+              alt="ERROR 404"
+            />
+          </div>
+          <div className="img no-select">
+            <Image
+              height={400}
+              width={400}
+              src="/stack/css-3.png"
+              alt="ERROR 404"
+            />
+          </div>
+          <div className="img no-select">
+            <Image
+              height={400}
+              width={400}
+              src="/stack/js.png"
+              alt="ERROR 404"
+            />
+          </div>
+          <div className="img no-select">
+            <Image
+              height={400}
+              width={400}
+              src="/stack/node.png"
+              alt="ERROR 404"
+            />
+          </div>
+          <div className="img no-select">
+            <Image
+              height={400}
+              width={400}
+              src="/stack/react2.png"
+              alt="ERROR 404"
+            />
+          </div>
+          <div className="img no-select">
+            <Image
+              className="scale-[1.3]"
+              height={400}
+              width={400}
+              src="/stack/Next.js.png"
+              alt="ERROR 404"
+            />
+          </div>
+          <div className="img no-select">
+            <Image
+              height={400}
+              width={400}
+              src="/stack/git.png"
+              alt="ERROR 404"
+            />
+          </div>
+          <div className="img no-select">
+            <Image
+              height={400}
+              width={400}
+              src="/stack/github.png"
+              alt="ERROR 404"
+            />
+          </div>
+          <div className="img no-select">
+            <Image
+              height={400}
+              width={400}
+              src="/stack/mongo.png"
+              alt="ERROR 404"
+            />
+          </div>
+          <div className="img no-select">
+            <Image
+              height={400}
+              width={400}
+              src="/stack/azure.png"
+              alt="ERROR 404"
+            />
+          </div>
         </div>
       </div>
-      <Link target='_blank' href={"https://github.com/Brijendra-Singh2003/BootCamp_2.0"}> <button id='signn' className={"no-select " + lilitaOne.className}> View GitHub Repository </button> </Link>
+      <Link
+        target="_blank"
+        href={"https://github.com/Brijendra-Singh2003/BootCamp_2.0"}
+      >
+        {" "}
+        <button id="signn" className={"no-select " + lilitaOne.className}>
+          {" "}
+          View GitHub Repository{" "}
+        </button>{" "}
+      </Link>
     </>
-  )
+  );
 }
 
 export const metadata = {
--- components/devCard/DevCard.js
+++ components/devCard/DevCard.js
@@ -1,20 +1,23 @@
-import Image from 'next/image'
-import React from 'react'
-import styles from "./devCard.module.css"
+import Image from "next/image";
+import React from "react";
 
-export default async function DevCard({src, name, id, children}) {
+export default async function DevCard({ src, name, id, children }) {
   return (
-    <div className={styles.card}>
-        <div className={styles.top}>
-            <Image src={src} height={200} width={200} priority alt='name' />
-        </div>
-        <div className={styles.bottom}>
-            <h1 className={styles.name}>{name}</h1>
-            <span className={styles.id}>{id}</span>
-            <div className={styles.contri}>
-            {children}
-            </div>
-        </div>
+    <div className="w-full mx-3 md:w-64 justify-center items-center bg-[#9b65ff33] border border-gray-600 bg-opacity-30 shadow-lg rounded-lg flex flex-col">
+      <Image
+        src={src}
+        height={400}
+        width={400}
+        alt="img"
+        title="img"
+        className="w-full max-w-xs aspect-square h-auto object-cover rounded-t-lg"
+      />
+      <div className="w-full px-4 py-2 justify-start flex flex-col">
+        <h4 className="font-semibold text-white text-lg">
+          {name} - {id}
+        </h4>
+        <p className="my-4 text-gray-300">{children}</p>
+      </div>
     </div>
-  )
+  );
 }
